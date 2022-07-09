9ae0aa2f9bdffef0b429b935429219877a225112
Masoudi Parrot <masoudi@localhost.localdomain>
2022-07-08 23:05:30 -0400

    write test for user modal

--- spec/modal/user_spec.rb
+++ spec/modal/user_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     subject { User.new(name: 'Harry', photo: 'photo/link.com', bio: 'description', postsCounter: 0)}
+    let (:post) { Post.create(user: subject, title: 'Hello', text: 'This is my first post', commentsCounter: 0, likesCounter: 0) }
 
-    before { subject.save}
+    before do
+        subject.save
+        post.save
+    end
+
     it 'User name should be present' do
         subject.name = 'Harry'
         expect(subject).to be_valid
@@ -13,9 +18,14 @@ RSpec.describe User, type: :model do
         subject.name = 't'
         expect(subject).to_not be_valid
     end
-    
+
     it 'PostsCounter should be an integer greater than or equal to zero' do
         subject.postsCounter = 'counter'
         expect(subject).to_not be_valid
+    end
+
+    it 'Should sho most recent posts' do
+        posts = subject.recent_post
+        expect(posts.include?(post)).to be_truthy
     end
 end
