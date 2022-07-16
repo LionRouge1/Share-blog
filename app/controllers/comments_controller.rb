7de440e131485317913a43bb7ad96f80c2c5cd3a
Masoudi Parrot <masoudi@localhost.localdomain>
2022-07-16 11:29:53 -0400

    Create comments controler

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+
+    def new
+        @comment = Comment.new
+        @post = Post.find(params[:post_id])
+    end
+
+    def create
+        @comment = Comment.create(comment_params)
+        @comment.user = @user
+        @post = Post.find(params[:post_id])
+        @comment.post = @post
+        puts 'tototo'
+        if @comment.save
+            redirect_to user_post_path(@user.id, @post.id), notice: "comment added successfully"
+        else
+            redirect_to user_post_path(@user.id, @post.id), alert: 'Comment failed to save.'
+        end
+    end
+
+    private 
+    
+    def comment_params
+        params.require(:comment).permit(:text)
+    end
+end
