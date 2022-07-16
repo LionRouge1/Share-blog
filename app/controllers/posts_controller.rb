eea1ce5841099c19e046c93e77e6a0e9d5afca4d
Masoudi Parrot <masoudi@localhost.localdomain>
2022-07-16 11:30:43 -0400

    add new method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = Comment.where(post: @post)
   end
+
+  def new
+    @post = Post.new
+    @user = User.find(params[:user_id])
+  end
+
+  def create 
+    @post = Post.create(post_params)
+    @post.user = @user
+    @post.likesCounter = 0
+    @post.commentsCounter = 0
+
+    if @post.save
+      redirect_to user_posts_path(@post.user, @post), notice: 'New post created successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
