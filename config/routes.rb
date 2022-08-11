1475de1133484808fdff40a004a48e8c3f73904f
Masoudi Parrot <masoudi@localhost.localdomain>
2022-08-11 10:27:34 -0400

    Remove blank lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   scope 'users/:user_id', as: 'user' do
     resources :posts, only: [:index, :show]
   end
