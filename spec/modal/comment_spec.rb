154432046f24a6472537f4db23cf2e2f6f02ad76
Masoudi Parrot <masoudi@localhost.localdomain>
2022-08-13 06:37:43 -0400

    test comment

--- spec/modal/comment_spec.rb
+++ spec/modal/comment_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :modal do
+
   let(:user) { User.create(name: 'Atawu', photo: 'https://unsplash.com/photos/F_ksks', bio: 'Teacher from Ghana.', postsCounter: 0) }
   let(:post) do
     Post.create(user: user, title: 'Hello', text: 'This is my first post', commentsCounter: 0, likesCounter: 0)
@@ -16,4 +17,5 @@ RSpec.describe Comment, type: :modal do
     comment.update_comment_counter
     expect(post.commentsCounter).to eq(1)
   end
+  
 end
