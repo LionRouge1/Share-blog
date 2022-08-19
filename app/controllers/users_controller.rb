dd8e0803dd442a86492ebda9a2a057aff217e2a8
Masoudi Parrot <masoudi@localhost.localdomain>
2022-08-19 11:40:37 -0400

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
+  # fetch all the users from the database
     @users = User.all
   end
 
   def show
+  # find user base on params
     @user = User.find(params[:id])
     @posts = Post.where(user: @user)
   end
