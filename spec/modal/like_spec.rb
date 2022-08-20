9bc9bcdb02a5521b8b87b1aafa09bd57c2b5b9bf
Masoudi Parrot <masoudi@localhost.localdomain>
2022-08-20 09:42:47 -0400

    spec test for like controller

--- spec/modal/like_spec.rb
+++ spec/modal/like_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :modal do
-  let(:user) { User.create(name: 'Atawu', photo: 'https://unsplash.com/photos/F_ksks', bio: 'Teacher from Ghana.', postsCounter: 0) }
+  # spec test for like controller
+  let(:user) do
+     User.create(name: 'Atawu', photo: 'https://unsplash.com/photos/F_ksks', bio: 'Teacher from Ghana.', postsCounter: 0)
+  end
   let(:post) do
     Post.create(user: user, title: 'Hello', text: 'This is my first post', commentsCounter: 0, likesCounter: 0)
   end
