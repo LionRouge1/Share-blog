c25ff774e1d93647cea9fc8ce7670dca207d591d
Masoudi Parrot <masoudi@localhost.localdomain>
2022-10-12 14:33:38 -0400

    overwrite device default parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
+  protect_from_forgery prepend: true
 
-  def current_user
-    @user = User.first
+  # before_action :current_user
+  before_action :authenticate_user!
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :photo, :bio, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :photo, :bio, :password, :current_password)
+    end
   end
 end
