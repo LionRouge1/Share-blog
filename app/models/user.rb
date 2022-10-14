dd9a54e50bfc4346fae506b23d9fc30760d53478
Masoudi Parrot <masoudi@localhost.localdomain>
2022-10-14 09:10:13 -0400

    remove method avatar_update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,26 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_one_attached :avatar
-
-  def avatar_update
-    avatar
-  end
+  after_commit :add_default_avatar, on: %i[create update]
 
   def recent_post
     posts.order(created_at: :desc).limit(3)
   end
 
   validates :name, presence: true, length: { in: 2..100, message: 'Should be in range of 2 to 100' }
+
+  private
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.json(
+            'app', 'assets', 'images', 'default_avatar.png'
+          ),
+          filename: 'default_avatar.png',
+          content_type: 'image/png'
+        )
+      )
+    end
+  end
 end
