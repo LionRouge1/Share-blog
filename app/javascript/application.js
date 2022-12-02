8356a228daeea24c9463e744469f844be7bd8337
Masoudi Parrot <masoudi@localhost.localdomain>
2022-12-02 16:34:14 -0500

    show uploaded image on the form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// show avatar when on registration when avatar change
+const avatar_input = document.getElementById('avatar_input');
+const avatar_box = document.getElementById('avatar_box');
+
+avatar_input.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', function() {
+        const uploaded_image = reader.result;
+        avatar_box.innerHTML = `<img src=${uploaded_image} title="avatar" >`;
+    });
+    console.log(reader);
+    reader.readAsDataURL(this.files[0]);
+})
